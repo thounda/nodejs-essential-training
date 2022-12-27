971cca2c40dff5afa9637c8f1577431c4446e18a
Thounda Craig <thoundac94@gmail.com>
2022-12-27 13:19:28 -0500

    initial commits

---
+++ Ch02/02_01/start/global.js
@@ -0,0 +1,4 @@
+// Example script to show how NodeJs executed to parse 'let' varibles at runtime
+let hello = "Hello World from Node.js";
+let justNode = hello.slice(17);
+console.log(`Who let the ${justNode} out?`);
