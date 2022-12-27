4d219ae1aebc98d62bb0cbc23afb597cf67a9500
Thounda Craig <thoundac94@gmail.com>
2022-12-27 15:48:52 -0500

    More examples while exploring Global

--- Ch02/02_02/start/global.js
+++ Ch02/02_02/start/global.js
@@ -1,3 +1,12 @@
-let hello = "Hello World from Node.js";
-let justNode = hello.slice(17);
-console.log(`Who let the ${justNode} out?`);
+// set PATH function to constant to use in script
+const path = require("path");
+
+// Get path to directory of current location
+console.log(__dirname);
+// Get the file name using the PATH argument to strip results just to the file name (itself)
+console.log(`The file name is ${path.basename(__filename)}`);
+
+// To take a look at the keys within the (NodeJS) global object
+for (let key in global) {
+  console.log(key);
+}
