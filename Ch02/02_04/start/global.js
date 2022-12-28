3741dc959b0e9f9a6d0802c66a3895e013657454
Thounda Craig <thoundac94@gmail.com>
2022-12-28 11:45:45 -0500

    Node example using Standard In / Out puts

--- Ch02/02_04/start/global.js
+++ Ch02/02_04/start/global.js
@@ -1,10 +1,31 @@
-function grab(flag) {
-  let indexAfterFlag = process.argv.indexOf(flag) + 1;
-  return process.argv[indexAfterFlag];
+/*
+Standard Input and Standard Output objects offer us a way to communicate with a process while it's running
+*/
+
+// Below line sends string to the terminal
+process.stdout.write("Hello \n\n");
+
+// Next, we can create an array of questions using a constant
+const questions = [
+  "What is your name?",
+  "What would you rather be doing?",
+  "What is your preferred programming language?",
+];
+
+// Create empty array to capture answers from the questions array
+const answers = [];
+
+// Create function named 'ask' to manage the questions and answers
+// The argument 'i' is for the index for each of these questions
+function ask(i) {
+  process.stdout.write(`\n\n\n ${questions[i]}`);
+  process.stdout.write(` > `);
 }
 
-let greeting = grab("--greeting");
-let user = grab("--user");
+// listen for a data event on this object using a function
+process.stdin.on("data", function (data) {
+  process.stdout.write(data.toString().trim());
+});
 
-console.log(greeting);
-console.log(user);
+// Invoke the questions
+ask(answers.length);
