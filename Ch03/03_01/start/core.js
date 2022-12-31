121f48d65d82caf28c59e8a5ac704d4f02d47ead
Thounda Craig <thoundac94@gmail.com>
2022-12-31 13:10:29 -0500

    Overview of NodeJS core modules

--- Ch03/03_01/start/core.js
+++ Ch03/03_01/start/core.js
@@ -1,7 +1,7 @@
 // The REQUIRE function is used to load NodeJS modules - not required to be installed
 const path = require("path"); // Path module is used to isolate a file from full path
 const util = require("util"); // util module used to extract details on an object
-const v8 = require("v8"); // v8 module provides details on resources - i.e. memory
+const v8 = require("v8"); // v8 module provides details on resources
 
 console.log(path.basename(__filename));
 const dirUploads = path.join("www", "files", "uploads"); // Join module is used to create a path (append)
