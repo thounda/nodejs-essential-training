eb48d541f5712584cf32b38fbf08f66258027472
Thounda Craig <thoundac94@gmail.com>
2022-12-31 09:53:51 -0500

    NodeJS file update to add reporting to terminal

--- Ch02/02_08/start/global.js
+++ Ch02/02_08/start/global.js
@@ -1,13 +1,26 @@
+// Create a wait time for the function using setTimeout
 const waitTime = 3000;
 console.log(`setting a ${waitTime / 1000} second delay`);
-const timerFinished = () => console.log("done");
-
-setTimeout(timerFinished, waitTime);
-
-const waitInterval = 500;
-let currentTime = 0;
-const incTime = () => {
-  currentTime += waitInterval;
+const timerFinished = () => {
+  clearInterval(interval);
+  console.log(`\nDone!`);
 };
 
+// Callback function to figger event
+setTimeout(timerFinished, waitTime);
+
+// Create Interval timer
+const waitInterval = 500;
+let currentTime = 0;
+
+// Set arrow function to trigger the count which now reports data to the terminal
+const incTime = () => {
+  currentTime += waitInterval;
+  const p = Math.floor((currentTime / waitInterval) * 100);
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`waiting... ${p}`);
+};
+
+// Invoke the arrow function with setInterval to count up to 3 seconds then quit
 const interval = setInterval(incTime, waitInterval);
