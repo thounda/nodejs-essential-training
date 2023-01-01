9022ff6cd4858a4de5a8d91d7f894eb1ba239f8b
Thounda Craig <thoundac94@gmail.com>
2023-01-01 16:57:31 -0500

    Basic NodeJS example or Readline function

---
+++ Ch03/03_02/start/ask.js
@@ -0,0 +1,12 @@
+// Import Readline function
+const readline - require("readline");
+// Create a warpper / interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Ask a question and get answer in console
+rl.question("How do you like Node?", answer => {
+    console.log(`Your answer: ${answer}`);
+});
