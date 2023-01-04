c83794514793c407b2613130255c9fe4241d922e
Thounda Craig <thoundac94@gmail.com>
2023-01-04 16:58:19 -0500

    Add NodeJS emitter catch answers

--- Ch03/03_07/start/ask.js
+++ Ch03/03_07/start/ask.js
@@ -1,20 +1,20 @@
+// Ask.js
+// Import module in from lib folder
 const collectAnswers = require("./lib/collectAnswers");
 
+// Create questions array to pass into a function
 const questions = [
   "What is your name?",
   "Where do you live?",
-  "What are you going to do with Node.js?"
+  "What are you going to do with Node.js?",
 ];
 
-const answerEvents = collectAnswers(
-  questions,
-  (answers) => {
-    console.log("Thank you for your answers!");
-    console.log(answers);
-    process.exit();
-  }
-);
+// Create a function to collect answers
+const answerEvents = collectAnswers(questions, (answers) => {
+  console.log("Thank you for your answers!");
+  console.log(answers);
+  process.exit();
+});
 
-answerEvents.on("answer", (answer) =>
-  console.log(`The answer is ${answer}`)
-);
+//Call the custom event
+answerEvents.on("answer", (answer) => console.log(`The answer is ${answer}`));
