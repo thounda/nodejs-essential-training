ce0ee6d0c5b39e336a01ade74d96afb24ef928b8
Thounda Craig <thoundac94@gmail.com>
2023-01-04 20:42:59 -0500

    NodeJS FS ReadFiles example

--- Ch04/04_02/start/list.js
+++ Ch04/04_02/start/list.js
@@ -1,10 +1,13 @@
+// Import FS module
 const fs = require("fs");
 
-fs.readdir("./", function (err, files) {
-  if (err) {
-    throw err;
-  }
-  console.log(files);
+/* Read file synchronously - ensure all lines comment out if reading file, asynchronously */
+// let ipsum = fs.readFileSync("./readme.md", "utf-8");
+//  console.log(ipsum);
+
+// Read file asynchronously - ensure all line comment out if running file synchronous method.
+fs.readFile("./readme.md", "utf-8", (err, ipsum) => {
+  console.log(ipsum);
 });
 
-console.log("reading files...");
+console.log("reading the file...");
